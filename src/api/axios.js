85ff92d97c4ad45e8afa2ba2cb38ee5258b7eb95
Phạm Quốc Tự <quoctu.se@gmail.com>
2026-03-09 13:33:21 +0700

    fix timeout

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,7 +5,6 @@ const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/';
 // Create axios instance
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
